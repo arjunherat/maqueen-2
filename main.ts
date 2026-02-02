fc26aed5271fec40a0cbc319bc2907c1426d45ca
arjunherat <arjunherat12@hotmail.com>
2026-02-02 12:59:37 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, 100)
-    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
-    basic.pause(2000)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 74)
+    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 74)
+    basic.pause(500)
     Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
     basic.pause(5000)
 })
